1d3bc4273e4867e18f421f743a565e9c2a487947
Uktamjon Komilov <uktamjonpocox@gmail.com>
2023-12-14 20:43:46 +0500

    added regions as districts

--- apps/regions/utils.py
+++ apps/regions/utils.py
@@ -20,8 +20,68 @@ def get_or_create_districts():
 
 DISTRICTS = [
     {
-        "name": "Toshkent",
-        "longitude": 40.4018,
-        "latitude": 71.4893,
+        "name": "Andijon viloyati",
+        "longitude": 72.166667,
+        "latitude": 40.75,
+    },
+    {
+        "name": "Buxoro viloyati",
+        "longitude": 63.666667,
+        "latitude": 40.166667,
+    },
+    {
+        "name": "Fargʻona viloyati",
+        "longitude": 71.35,
+        "latitude": 40.43,
+    },
+    {
+        "name": "Jizzax viloyati",
+        "longitude": 67.666667,
+        "latitude": 40.416667,
+    },
+    {
+        "name": "Xorazm viloyati",
+        "longitude": 61,
+        "latitude": 41.333333,
+    },
+    {
+        "name": "Namangan viloyati",
+        "longitude": 71.166667,
+        "latitude": 41,
+    },
+    {
+        "name": "Navoiy viloyati",
+        "longitude": 64.25,
+        "latitude": 42,
+    },
+    {
+        "name": "Qashqadaryo viloyati",
+        "longitude": 66.083333,
+        "latitude": 38.833333,
+    },
+    {
+        "name": "Qoraqalpogʻiston Respublikasi",
+        "longitude": 58.86,
+        "latitude": 43.04,
+    },
+    {
+        "name": "Samarqand viloyati",
+        "longitude": 66.25,
+        "latitude": 39.833333,
+    },
+    {
+        "name": "Sirdaryo viloyati",
+        "longitude": 68.666667,
+        "latitude": 40.416667,
+    },
+    {
+        "name": "Surxondaryo viloyati",
+        "longitude": 67.5,
+        "latitude": 38,
+    },
+    {
+        "name": "Toshkent viloyati",
+        "longitude": 69.75,
+        "latitude": 41.166667,
     },
 ]
